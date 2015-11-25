05824d2f9eb4497d22abae58520b3522a0ed3028
Jason Tarasovic <jason@tarasovic.com>
2015-11-25 22:31:40 +0400

    downgrade exrm

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule IpFinder.Mixfile do
       {:phoenix_live_reload,  "~> 1.0", only: :dev},
       {:cowboy,               "~> 1.0"},
       {:httpoison,            "~> 0.8.0"},
-      {:exrm,                 "~> 1.0.0-rc5"}
+      {:exrm,                 "~> 0.19.9"}
    ]
   end
 
